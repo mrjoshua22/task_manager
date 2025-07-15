b71386467c8470b1b5d08f92411ca302a8fc3dc1
mrjoshua22 <ulanov.ng@gmail.com>
2025-07-15 03:55:16 +0300

    test: add tests for models factories

---
+++ test/factories/sequence.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  sequence(:string, aliases: %i[
+    first_name
+    last_name
+    password
+    email
+    avatar
+    type
+    name
+    description
+    state
+    expired_at
+  ]) do |n|
+    "string#{n}"
+  end
+end
