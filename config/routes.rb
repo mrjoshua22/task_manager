f1e06a5993c6ec0070970aee68ccaf3cc30bb6ca
mrjoshua22 <ulanov.ng@gmail.com>
2025-07-21 17:07:15 +0300

    feat: add developers controller with new and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   scope module: :web do
     resource :board, only: :show
     resource :session, only: :new
+
+    resources :developers, only: [:new, :create]
   end
 end
