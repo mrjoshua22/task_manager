7b0ea5637073382297250d6fc3eeb9fde01f014f
mrjoshua22 <ulanov.ng@gmail.com>
2025-07-25 01:40:43 +0300

    feat: add update action

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -4,6 +4,10 @@ class Api::V1::ApplicationController < Api::ApplicationController
   include AuthHelper
   helper_method :current_user
 
+  def self.responder
+    JsonResponder
+  end
+
   def build_meta(collection)
     {
       count: collection.count,
