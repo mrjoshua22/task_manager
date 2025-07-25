dae86115b57a59dde9acb0efad358256c4cf00c5
mrjoshua22 <ulanov.ng@gmail.com>
2025-07-25 20:41:30 +0300

    feat: move respond_to from api tasks controller to api application controller

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ApplicationController < Api::ApplicationController
   include AuthHelper
   helper_method :current_user
+  
+  respond_to :json
 
   RANSACK_DEFAULT_SORT = 'id ASC'.freeze
 
