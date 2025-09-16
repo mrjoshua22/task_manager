04158a2514937ab6b601ffd26c86fc00ced7da4b
mrjoshua22 <ulanov.ng@gmail.com>
2025-09-16 14:19:57 +0300

    fix: add default json format to api routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :tasks, only: [:index, :show, :create, :update, :destroy]
+      resources :tasks, only: [:index, :show, :create, :update, :destroy],
+        defaults: { format: :json }
     end
   end
 
