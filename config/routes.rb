d5a6591652da8316dc8670abb4cbeb83973c6a4c
mrjoshua22 <ulanov.ng@gmail.com>
2025-09-21 23:07:13 +0300

    fix: change default format for api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ Rails.application.routes.draw do
     resources :users
   end
 
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :tasks, only: [:index, :show, :create, :update, :destroy],
-        defaults: { format: :json }
+      resources :tasks, only: [:index, :show, :create, :update, :destroy]
     end
   end
 
